04c67c01ee846ab72533a04a768ff88935a2a1b9
DRobinson4105 <drobinson4105@gmail.com>
2019-10-12 15:40:31 -0400

    Color Sensor
    I made the methods for the RGB colors.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestColorSensor.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class TestColorSensor {
+    ColorSensor Color;
+
+    public int Red() {
+        return Color.red();
+    }
+
+    public int Green() {
+        return Color.green();
+    }
+
+    public int Blue() {
+        return Color.blue();
+    }
+}

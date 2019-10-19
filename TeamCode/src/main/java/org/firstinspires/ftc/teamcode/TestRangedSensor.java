b23b37a8551054ab0a768d905197d6a3d1a451d5
Nakas88 <farhanmahbub88@gmail.com>
2019-10-19 16:38:26 -0400

    Fixed some mistakes
    mistakes have been fixed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRangedSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRangedSensor.java
@@ -1,15 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class TestRangedSensor {
     ModernRoboticsI2cRangeSensor Range;
-    double distance = Range.getDistance(DistanceUnit.INCH)
+    double distance = Range.getDistance(DistanceUnit.INCH);
 
     public void init(HardwareMap hardwareMap) {
-        Range = hardwareMap.get(TestRangedSensor.class, "RANGEd\D_SENSOR")
+        TestRangedSensor Range = hardwareMap.get(TestRangedSensor.class, "RANGED_SENSOR");
     }
 
 }

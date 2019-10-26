272069246653a099c07318b58ec1a195f5b3fc28
Alero Adegoroye <alero.adegoroye@gmail.com>
2019-10-26 08:27:48 -0400

    Auto for DriveTrain
    This is for the auto for the drivetrain and measures how far the drivetrain goes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAutoDriveTrain.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TestAutoDriveTrain {
+    DcMotor FRONT_LEFT;
+    DcMotor FRONT_RIGHT;
+    DcMotor BACK_LEFT;
+    DcMotor BACK_RIGHT;
+
+    int position = FRONT_LEFT.getCurrentPosition();
+
+    HardwareMap hardwareMap;
+
+
+    public void init(){
+        FRONT_LEFT = hardwareMap.dcMotor.get("FL");
+        FRONT_RIGHT = hardwareMap.dcMotor.get("FR");
+        BACK_LEFT = hardwareMap.dcMotor.get("BL");
+        BACK_RIGHT = hardwareMap.dcMotor.get("BR");
+
+        FRONT_RIGHT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FRONT_LEFT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BACK_RIGHT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BACK_LEFT.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+
+    public void drive(int distance , double power){
+
+
+        while(position <= distance){
+        FRONT_RIGHT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRONT_LEFT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BACK_RIGHT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BACK_LEFT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+
+        }
+    }
+
+
+
+
+
+}

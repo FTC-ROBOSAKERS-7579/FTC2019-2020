50b74986f9c760c4e60c90582a545c18229cb180
DRobinson4105 <drobinson4105@gmail.com>
2019-11-09 19:19:30 -0500

    Maker's Faire Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -9,19 +9,16 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TestTeleOp extends LinearOpMode {
     ElapsedTime runtime = new ElapsedTime();
     TestDriveTrain Dtrain = new TestDriveTrain();
-    TestCascade cascade = new TestCascade();
-    TestIntake Intake = new TestIntake();
-
-
-
+//    TestCascade cascade = new TestCascade();
+//    TestIntake Intake = new TestIntake();
 
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         Dtrain.init(hardwareMap);
-        cascade.init(hardwareMap);
-        Intake.init(hardwareMap);
+//        cascade.init(hardwareMap);
+//        Intake.init(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -34,26 +31,27 @@ public class TestTeleOp extends LinearOpMode {
             Dtrain.arcadeDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
 
             // INTAKE
-            if(gamepad1.x)
-                Intake.ARMIN(0.4, 0.2);
-
-            else if(gamepad1.y)
-                Intake.ARMOUT(0.4, 0.2);
-
-            else
-                Intake.ARMSTOP();
+//            if(gamepad1.x)
+//                Intake.ARMIN(0.4, 0.2);
+//
+//            else if(gamepad1.y)
+//                Intake.ARMOUT(0.4, 0.2);
+//
+//            else
+//                Intake.ARMSTOP();
 
             // CASCADE
-            if (gamepad1.dpad_up)
-                cascade.CasPow(0.4, -0.4);
-            else if (gamepad1.dpad_down)
-                cascade.CasPow(-0.4, 0.4);
-            else
-                cascade.CasPow(0, 0);
+//            if (gamepad1.dpad_up)
+//                cascade.CasPow(0.4, -0.4);
+//            else if (gamepad1.dpad_down)
+//                cascade.CasPow(-0.4, 0.4);
+//            else
+//                cascade.CasPow(0, 0);
+//
+//        }
 
         }
 
+
     }
-
-
 }

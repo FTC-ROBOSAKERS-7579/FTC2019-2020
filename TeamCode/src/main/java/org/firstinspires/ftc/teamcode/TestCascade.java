3c11cad0d2d5820912ff3846acea2d29ea13b811
DRobinson4105 <58113704+DRobinson4105@users.noreply.github.com>
2019-11-27 15:12:09 -0500

    Cascade Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCascade.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TestCascade {
+
+    DcMotor Cascade1;
+    DcMotor Cascade2;
+    HardwareMap hardwareMap;
+
+    public void init (HardwareMap hardwareMap) {
+
+    }
+}

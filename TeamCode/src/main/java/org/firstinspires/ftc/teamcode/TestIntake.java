80ac26c062defae61daa7f099c44a593ab95a121
DRobinson4105 <58113704+DRobinson4105@users.noreply.github.com>
2019-11-29 13:23:45 -0500

    intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
@@ -1,24 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class TestIntake {
 
-    Servo Intake1;
-    Servo Intake2;
+    DcMotor Intake;
 
     public void init(HardwareMap hardwareMap) {
-        Intake1 = hardwareMap.servo.get("INTAKE_1");
-        Intake2 = hardwareMap.servo.get("INTAKE_2");
-
-        Intake1.setDirection(Servo.Direction.FORWARD);
-        Intake2.setDirection(Servo.Direction.REVERSE);
+        Intake = hardwareMap.dcMotor.get("INTAKE_1");
     }
 
     public void Intake(double power) {
-        Intake1.setPosition(power);
-        Intake2.setPosition(-power);
+        Intake.setPower(power)
     }
 
 }

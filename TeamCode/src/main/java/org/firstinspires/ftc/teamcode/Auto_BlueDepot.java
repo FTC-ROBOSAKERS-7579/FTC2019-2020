be7711fbfb19261cc25980d41bcab9897efee141
DRobinson4105 <58113704+DRobinson4105@users.noreply.github.com>
2020-01-18 11:32:49 -0500

    New Autos for League Championship
    I created the new auto classes for league championships and started adding the pseudo code for them.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BlueDepot.java
@@ -0,0 +1,23 @@
+/*
+- Move forward to brick
+- Grab brick
+- Back up a little
+- Strafe left until aligned with middle of foundation
+- Move forward a little
+- Drop brick
+- Back up a little
+- Strafe right until aligned with third brick
+- Move forward a little
+- Grab brick
+- Back up a little
+- Strafe left until aligned with right part of foundation
+- Move forward a little
+- Drop block
+- Back up a little
+- Strafe right until under bridge
+*/
+
+package org.firstinspires.ftc.teamcode;
+
+public class Auto_BlueDepot {
+}

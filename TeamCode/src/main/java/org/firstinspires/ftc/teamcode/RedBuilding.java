67880e4ebf8aef9052fd6d8933877aaa413bb05c
Alero Adegoroye <alero.adegoroye@gmail.com>
2020-02-01 22:25:30 -0500

    More Autos are added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBuilding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBuilding.java
@@ -1,32 +1,34 @@
 /*
 - Move forward a little
-- Strafe right until aligned with first block
+- Strafe left until aligned with first block
 - Move forward to brick
 - Grab brick
 - Back up a little
-- Strafe left until aligned with middle of foundation
+- Strafe right until aligned with middle of foundation
 - Move forward a little
 - Drop brick
 - Back up a little
-- Strafe right until aligned with third brick
+- Strafe left until aligned with third brick
 - Move forward a little
 - Grab brick
 - Back up a little
-- Strafe left until aligned with right part of foundation
+- Strafe right until aligned with right part of foundation
 - Move forward a little
 - Drop block
 - Back up a little
-- Strafe right until under bridge
+- Strafe left until under bridge
 */
 
-package org.firstinspires.ftc.teamcode;
+
+
+        package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "BlueBuilding")
-public class AutoBlueBuilding extends LinearOpMode {
+@Autonomous(name = "RedBuilding")
+public class RedBuilding extends LinearOpMode {
     AutoDrivetrain drivetrain = new AutoDrivetrain();
     Intake intake = new Intake();
     Cascade cascade = new Cascade();
@@ -41,41 +43,53 @@ public class AutoBlueBuilding extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        waitForStart();
-        runtime.reset();
-
-        // Move forward a little
-        drivetrain.drive(10, 0.5);
+        cascade.autoCascade(5,0.2);
         runtime.reset();
         sleep(500);
 
-        // Strafe right until aligned with first block
-        drivetrain.strafe(48, 0.5);
+        // Move forward a little
+        drivetrain.drive(12, 0.2);
+        runtime.reset();
+        sleep(500);
+
+        waitForStart();
+        runtime.reset();
+
+        // Strafe left until aligned with first block
+        drivetrain.strafe(-48, 0.2);
         runtime.reset();
         sleep(500);
 
         // Move forward to brick
-        drivetrain.drive(24, 0.5);
+        drivetrain.drive(11, 0.2);
         runtime.reset();
         sleep(500);
 
         // Grab brick
+        intake.open();
+        runtime.reset();
+        sleep(500);
+
+        drivetrain.drive(1, 0.2);
+        runtime.reset();
+        sleep(500);
+
         intake.grab();
         runtime.reset();
         sleep(500);
 
         // Back up a little
-        drivetrain.drive(-10, 0.5);
+        drivetrain.drive(-11, 0.2);
         runtime.reset();
         sleep(500);
 
-        // Strafe left until aligned with middle of foundation
-        drivetrain.strafe(-72, 0.5);
+        // Strafe right until aligned with middle of foundation
+        drivetrain.strafe(72, 0.2);
         runtime.reset();
         sleep(500);
 
         // Move forward a little
-        drivetrain.drive(10, 0.5);
+        drivetrain.drive(12, 0.2);
         runtime.reset();
         sleep(500);
 
@@ -85,37 +99,45 @@ public class AutoBlueBuilding extends LinearOpMode {
         sleep(500);
 
         // Back up a little
-        drivetrain.drive(-10, 0.5);
+        drivetrain.drive(-12, 0.2);
         runtime.reset();
         sleep(500);
 
-        // Strafe right until aligned with third brick
-        drivetrain.strafe(88, 0.5);
+        // Strafe left until aligned with third brick
+        drivetrain.strafe(-96, 0.2);
         runtime.reset();
         sleep(500);
 
         // Move forward a little
-        drivetrain.drive(10, 0.5);
+        drivetrain.drive(11, 0.2);
         runtime.reset();
         sleep(500);
 
         // Grab brick
+        intake.open();
+        runtime.reset();
+        sleep(500);
+
+        drivetrain.drive(1, 0.2);
+        runtime.reset();
+        sleep(500);
+
         intake.grab();
         runtime.reset();
         sleep(500);
 
         // Back up a little
-        drivetrain.drive(-10, 0.5);
+        drivetrain.drive(-12, 0.2);
         runtime.reset();
         sleep(500);
 
-        // Strafe left until aligned with right part of foundation
-        drivetrain.strafe(-72, 0.5);
+        // Strafe right until aligned with right part of foundation
+        drivetrain.strafe(96, 0.2);
         runtime.reset();
         sleep(500);
 
         // Move forward a little
-        drivetrain.drive(10, 0.5);
+        drivetrain.drive(11, 0.2);
         runtime.reset();
         sleep(500);
 
@@ -125,15 +147,20 @@ public class AutoBlueBuilding extends LinearOpMode {
         sleep(500);
 
         // Back up a little
-        drivetrain.drive(-10, 0.5);
+        drivetrain.drive(-11, 0.2);
         runtime.reset();
         sleep(500);
 
-        // Strafe right until under bridge
-        drivetrain.strafe(24, 0.5);
+        // Strafe left until under bridge
+        drivetrain.strafe(-48, 0.2);
         runtime.reset();
         sleep(500);
+
 
     }
 
 }
+
+
+
+
